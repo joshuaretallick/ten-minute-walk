49dc46ca9dbb1284379f31571f9352c6666afafe
Joshua Retallick <josh@old-empire.co.uk>
2020-12-28 19:52:18 +0000

    Solved problem to check user returns to starting point

--- lib/walk.rb
+++ lib/walk.rb
@@ -1,7 +1,25 @@
+
 def ten_minute_walk(directions)
+  vertical = 0
+  horizontal = 0
+
+  directions.each do |x|
+    if x == 'n'
+      vertical += 1
+    elsif x == 's'
+      vertical -= 1
+    elsif x == 'w'
+      horizontal += 1
+    elsif x == 'e'
+      horizontal -= 1
+    end
+  end
+
   if directions.length < 10 || directions.length > 10
     return false
-  elsif directions.length == 10
+  elsif directions.length == 10 && vertical == 0 && horizontal == 0
     return true
+  else
+    return false
   end
 end
--- spec/walk_spec.rb
+++ spec/walk_spec.rb
@@ -17,7 +17,12 @@ describe 'ten minute walk' do
     expect(ten_minute_walk(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'])).to eq false
   end
 
-  it "returns true when given ten different inputs" do
-    expect(ten_minute_walk(['w', 's', 'e', 's', 'n', 's', 'w', 's', 'n', 's'])).to eq true
+  it "returns true when given ten correct inputs" do
+    expect(ten_minute_walk(['w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e'])).to eq true
   end
+
+  it "returns false when given ten incorrect inputs" do
+    expect(ten_minute_walk(['w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'w'])).to eq false
+  end
+
 end
