fc4eb5f2c4b671f58e89e45946b421e9b4427a23
Joshua Retallick <josh@old-empire.co.uk>
2020-12-28 19:20:04 +0000

    Set up testing to recognise whether user has taken 10 steps and returns true, but false otherwise

--- lib/walk.rb
+++ lib/walk.rb
@@ -1,3 +1,7 @@
 def ten_minute_walk(directions)
-  false
+  if directions.length < 10 || directions.length > 10
+    return false
+  elsif directions.length == 10
+    return true
+  end
 end
--- spec/walk_spec.rb
+++ spec/walk_spec.rb
@@ -1,8 +1,23 @@
 require 'walk'
 
 describe 'ten minute walk' do
-  # 'n', 's' |     false
   it "returns false when given two inputs" do
     expect(ten_minute_walk(['n', 's'])).to eq false
+  end
+
+  it "returns true when given ten inputs" do
+    expect(ten_minute_walk(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'])).to eq true
+  end
+
+  it "returns false when given six inputs" do
+    expect(ten_minute_walk(['n', 's', 'n', 's', 'n', 's'])).to eq false
+  end
+
+  it "returns false when given twelve inputs" do
+    expect(ten_minute_walk(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'])).to eq false
+  end
+
+  it "returns true when given ten different inputs" do
+    expect(ten_minute_walk(['w', 's', 'e', 's', 'n', 's', 'w', 's', 'n', 's'])).to eq true
   end
 end
